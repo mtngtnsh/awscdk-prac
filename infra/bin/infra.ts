fb08bc924007a5a0b7c59a71b5af0000f9e5be33
mtngtnsh <soeryuan@gmail.com>
2022-10-29 12:01:11 +0900

    Added prettier.

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import * as cdk from 'aws-cdk-lib'
+import { InfraStack } from '../lib/infra-stack'
 
-const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
+const app = new cdk.App()
+new InfraStack(app, 'InfraStack')
--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,17 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as Infra from '../lib/infra-stack';
+import * as cdk from 'aws-cdk-lib'
+import { Template, Match } from 'aws-cdk-lib/assertions'
+import * as Infra from '../lib/infra-stack'
 
 test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
+  const app = new cdk.App()
   // WHEN
-  const stack = new Infra.InfraStack(app, 'MyTestStack');
+  const stack = new Infra.InfraStack(app, 'MyTestStack')
   // THEN
 
-  const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack)
 
   template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
-});
+    VisibilityTimeout: 300,
+  })
+  template.resourceCountIs('AWS::SNS::Topic', 1)
+})
